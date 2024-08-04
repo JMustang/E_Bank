f5c4eee4929225d16b2e91ce429fab581466c1b7
Junior <eecfredes@gmail.com>
2024-08-04 13:19:59 -0300

    :white_check_mark:Test Transfer - Get Transfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -3,8 +3,10 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/JMustang/E_Bank/util"
+	"github.com/stretchr/testify/require"
 )
 
 func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
@@ -26,4 +28,20 @@ func TestCreateTrasnfer(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccoubtID, transfer2.FromAccoubtID)
+	require.Equal(t, transfer1.ToAccoubtID, transfer2.ToAccoubtID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
 }
