d7e015b0bd56e65da673f1b38a29a5e27bed3cc4
Junior <eecfredes@gmail.com>
2024-08-04 13:31:33 -0300

    :white_check_mark:Test Transfer - List Transfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -45,3 +45,28 @@ func TestGetTransfer(t *testing.T) {
 	require.Equal(t, transfer1.Amount, transfer2.Amount)
 	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
 }
+
+func TestListTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+		createRandomTransfer(t, account2, account1)
+	}
+
+	arg := ListTransfersParams{
+		FromAccoubtID: account1.ID,
+		ToAccoubtID:   account1.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.True(t, transfer.FromAccoubtID == account1.ID || transfer.ToAccoubtID == account1.ID)
+	}
+}
