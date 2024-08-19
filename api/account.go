8bfbf52adcab83e575fd5d2f0c531ddf31f7568c
Junior <eecfredes@gmail.com>
2024-08-18 21:49:42 -0300

    :rocket: Delete account handler

--- api/account.go
+++ api/account.go
@@ -115,3 +115,28 @@ func (server *Server) updateAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, updatedAccount)
 }
+
+// Estrutura para a requisição de deleção de conta
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "account deleted"})
+}
