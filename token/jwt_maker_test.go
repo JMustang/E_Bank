d2456b8d1414d7b8cb648cba09d8bddf515f51ea
Junior <eecfredes@gmail.com>
2024-09-14 22:16:04 -0300

    :white_check_mark:Testing invalid JWT token

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/JMustang/E_Bank/util"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,5 +48,22 @@ func TestExpiredJWTToken(t *testing.T) {
 	payload, err = maker.VerifyToken(token)
 	require.Error(t, err)
 	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomOwner(), util.DepositorRole, time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
 	require.Nil(t, payload)
 }
