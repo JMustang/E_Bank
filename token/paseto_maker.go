cb8d1ce0fb6ea116b4048eda0ff3c4644c1519e7
Junior <eecfredes@gmail.com>
2024-09-15 17:06:08 -0300

    :rocket: Create token with paseto

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -2,6 +2,7 @@ package token
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/aead/chacha20poly1305"
 	"github.com/o1egl/paseto"
@@ -25,4 +26,15 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 	}
 
 	return maker, nil
+}
+
+// CreateToken creates a new token for a specific username and duration
+func (maker *PasetoMaker) CreateToken(username string, role string, duration time.Duration) (string, *Payload, error) {
+	payload, err := NewPayload(username, role, duration)
+	if err != nil {
+		return "", payload, err
+	}
+
+	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	return token, payload, err
 }
