3fd8a40fcfbff31d2245b6dc18fcfc6ef7a7407d
Junior <eecfredes@gmail.com>
2024-09-16 20:18:27 -0300

    :rocket: passing the token maker to server

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/JMustang/E_Bank/db/sqlc"
+	"github.com/JMustang/E_Bank/token"
+	"github.com/JMustang/E_Bank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,13 +13,23 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -36,7 +50,7 @@ func NewServer(store db.Store) *Server {
 
 	server.router = router
 	// Return the server instance
-	return server
+	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
