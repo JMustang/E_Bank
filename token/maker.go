75959a33f2826f7325154eac1ea7e3297414a954
Junior <eecfredes@gmail.com>
2024-09-19 20:38:13 -0300

    :rocket: Commenting parameter for testing

--- token/maker.go
+++ token/maker.go
@@ -5,7 +5,11 @@ import "time"
 // Interfave for managing tokens
 type Maker interface {
 	// CreateToken Creates a new token for a specific username and duration
-	CreateToken(username string, role string, duration time.Duration) (string, *Payload, error)
+	CreateToken(
+		username string,
+		// role string,
+		duration time.Duration,
+	) (string, *Payload, error)
 
 	// VerifyToken checks if the token is valid or not
 	VerifyToken(token string) (*Payload, error)
