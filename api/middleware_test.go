1ec47b8afb6bd74c27e83e857674daca81e2678d
Junior <eecfredes@gmail.com>
2024-09-20 21:40:33 -0300

    :rocket: A middleware for auth

---
+++ api/middleware_test.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/JMustang/E_Bank/token"
+	"github.com/gin-gonic/gin"
+)
+
+func TestAuthMiddleware(t *testing.T) {
+	testCases := []struct {
+		name          string
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			server := newTestServer(t, nil)
+
+			authPath := "/auth"
+			server.router.Get(
+				authPath,
+				authMiddleware(server.tokenMaker),
+				func(ctx *gin.Context) {
+					ctx.JSON(http.StatusOK, gin.H{})
+				},
+			)
+		})
+	}
+}
